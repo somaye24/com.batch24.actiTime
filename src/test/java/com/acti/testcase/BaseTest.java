5b8f4661c1b31cb694f23edafdee569a02319d4d
12018 <12018@LAPTOP-IHNEA3MF.mynetworksettings.com>
2023-05-01 08:25:10 -0700

    import enter page to BaseTest class

--- src/test/java/com/acti/testcase/BaseTest.java
+++ src/test/java/com/acti/testcase/BaseTest.java
@@ -4,16 +4,19 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.acti.base.DriverScript;
+import com.acti.pages.EnterPage;
 import com.acti.pages.LoginPage;
 
 public class BaseTest extends DriverScript {
 	
 	LoginPage lp;
+	EnterPage ep;
 	
 	@BeforeMethod
 	public void preTest() {
 		initApplication();
 		lp = new LoginPage();
+		ep = new EnterPage();
 	}
 	
 	@AfterMethod
