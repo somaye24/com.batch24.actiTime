f09efb7db5ad1824af406c3a29c57756839d21af
12018 <12018@LAPTOP-IHNEA3MF.mynetworksettings.com>
2023-05-01 04:53:25 -0700

    Add author datails

--- src/main/java/com/acti/pages/EnterPage.java
+++ src/main/java/com/acti/pages/EnterPage.java
@@ -2,6 +2,13 @@ package com.acti.pages;
 
 import com.acti.base.DriverScript;
 
+/* Name= Enter Page
+ * Description: Define Enter page datails and methods
+ * Create By: Somayeh
+ * Review: Shantosh
+ * Date of Review: 4/26/2023
+ */
+
 public class EnterPage extends DriverScript {
 
 }
